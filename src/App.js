4f893169e08418ec7ce323f2210d99843cdc9ead
Spencillian <spencerli253@gmail.com>
2023-10-07 15:43:24 -0400

    stuff

--- src/App.js
+++ src/App.js
@@ -1,22 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [pressed, setPressed] = useState(false);
+
+  // {
+  //   pressed,
+  //   setPressed(modVale);
+  // }
+
+  if (pressed) {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Pressed
+          </p>
+          <button onClick={() => setPressed(!pressed)}>
+            Click Me!
+          </button>
+        </header>
+      </div>
+    );
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello scottylabs
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={() => setPressed(!pressed)}>
+          Click Me!
+        </button>
       </header>
     </div>
   );
